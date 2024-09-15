d6316b4c57be5cd527fb912b9d1939e7f1fce390
Ryan Morgan <RyanOnGithub@gmail.com>
2024-09-14 19:40:39 -0700

    Create project structure and integrate Authentication via Clerk (provider)

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,28 @@
+import { Inter } from 'next/font/google';
+import { ClerkProvider } from '@clerk/nextjs';
+
+import '../globals.css';
+
+/* Helps with SEO */
+export const metadata = {
+  title: 'Threads',
+  description: 'A NextJS 14 Meta Threads Application',
+}
+
+const inter = Inter({ subsets: ['latin'] });
+
+export default function RootLayout({
+  children
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <ClerkProvider>
+      <html lang='en'>
+        <body className={ `${inter.className} bg-dark-1` }>
+          { children }
+        </body>
+      </html>
+    </ClerkProvider>
+  )
+}
