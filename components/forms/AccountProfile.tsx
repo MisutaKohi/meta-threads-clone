63825aac53b2dc25594571aa1da30c3942437cea
Ryan Morgan <RyanOnGithub@gmail.com>
2024-09-24 21:47:31 -0700

    Add basic form outline to onboarding page

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -1,5 +1,24 @@
 "use client";
 
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod";
+ 
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+
+import { UserValidation } from "@/lib/validations/user";
+
 interface Props {
   user: {
     id: string;
@@ -13,10 +32,43 @@ interface Props {
 }
 
 const AccountProfile = ({ user, btnTitle } : Props) => {
+
+  const form = useForm<z.infer<typeof UserValidation>>({
+    resolver: zodResolver(UserValidation),
+    defaultValues: {
+      profile_photo: '',
+      name: '',
+      username: '',
+      bio: '',
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof UserValidation>) {
+    console.log(values);
+  }
+
   return (
-    <div>
-      Account Profile
-    </div>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
   )
 }
 
